50977e0274c311d3317e1c2223073c5c5dcec6ff
osmocn [osmo-sian] <108524973+osmocn@users.noreply.github.com>
2025-11-22 13:22:44 +0530

    fix: verification update

--- app/api/verify-email/route.ts
+++ app/api/verify-email/route.ts
@@ -1,8 +1,17 @@
 import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 import env from "@/lib/env";
+import { EMAIL_VERIFICATION } from "@/lib/constants";
+import { notFound } from "next/navigation";
+import db from "@/app/_db/db";
+import { emailTable } from "@/app/_db/email-schema";
+import { eq } from "drizzle-orm";
 
 export const GET = async (req: Request) => {
+  if (!EMAIL_VERIFICATION) {
+    return notFound();
+  }
+
   const url = new URL(req.url);
   const token = url.searchParams.get("token");
 
@@ -16,6 +25,10 @@ export const GET = async (req: Request) => {
   try {
     // biome-ignore lint/style/noNonNullAssertion: verified statically
     const result = jwt.verify(token, env.JWT_SECRET!) as { email: string };
+    await db
+      .update(emailTable)
+      .set({ verifiedAt: new Date() })
+      .where(eq(emailTable.email, result.email));
     return NextResponse.redirect(
       `${env.NEXT_PUBLIC_APP_URL}/email-verified?email=${result.email}`,
     );
